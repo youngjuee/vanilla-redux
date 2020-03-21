d9721d407a3e1c15bc08be41e7e662815257cba6
youngjuee <57988260+youngjuee@users.noreply.github.com>
2020-03-22 02:39:37 +0900

    simple counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let count = 0;
+number.innerText = count;
+const updateText = () => {
+  number.innerText = count;
+};
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
