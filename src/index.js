179aa520510b8293110fad8e5effb16e2ccd1d6d
youngjuee <57988260+youngjuee@users.noreply.github.com>
2020-03-22 03:09:11 +0900

    simple counter2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
+import { createStore } from "redux";
+// 리덕스는 데이터를 관리하기위해 만들어졌다.
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  console.log(action, count);
+  if (action.type === "ADD") {
+    count = count + 1;
+  } else if (action.type === "MINUS") {
+    count = count - 1;
+  } else {
+    return count;
+  }
+  return count;
 };
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+console.log(countStore.getState());
