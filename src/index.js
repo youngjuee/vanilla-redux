77ecded416135e787f7b18d80df38ec1123e8462
youngjuee <57988260+youngjuee@users.noreply.github.com>
2020-03-22 11:55:15 +0900

    코드 줄이기

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(action, count);
-  if (action.type === "ADD") {
-    count = count + 1;
-  } else if (action.type === "MINUS") {
-    count = count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case "ADD":
+      return (count = count + 1);
+    case "MINUS":
+      return (count = count - 1);
+    default:
+      return count;
   }
-  return count;
 };
 const countStore = createStore(countModifier);
 const onChange = () => {
@@ -22,10 +25,10 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
